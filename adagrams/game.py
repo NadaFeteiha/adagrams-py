b08374c7e0b0809d418aae5df627f1a67cf7aa8e
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-09 15:10:16 -0700

    implement score_word wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,25 +1,46 @@
 from random import randint
-import collections 
+import collections
 
 # global pool of letters
 POOL = {
-    "A": 9, "B": 2, "C": 2, "D": 4, 
-    "E": 12,  "F": 2,  "G": 3,  "H": 2, 
-    "I": 9 , "J": 1,  "K":1,  "L": 4, 
-    "M": 2, "N": 6, "O": 8 , "P": 2, "Q": 1,
-    "R":6, "S" : 4, "T": 6, "U":4, "V": 2, "W":2 
-    , "X": 1, "Y":2, "Z": 1 
+    "A": 9,
+    "B": 2,
+    "C": 2,
+    "D": 4,
+    "E": 12,
+    "F": 2,
+    "G": 3,
+    "H": 2,
+    "I": 9,
+    "J": 1,
+    "K": 1,
+    "L": 4,
+    "M": 2,
+    "N": 6,
+    "O": 8,
+    "P": 2,
+    "Q": 1,
+    "R": 6,
+    "S": 4,
+    "T": 6,
+    "U": 4,
+    "V": 2,
+    "W": 2,
+    "X": 1,
+    "Y": 2,
+    "Z": 1,
 }
 
 SCORE_WORD = {
     1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
     2: ["D", "G"],
     3: ["B", "C", "M", "P"],
-    4: ["F" ,"H", "V", "W", "Y"],
-    5:["K"],
-    8:["J", "X"],
-    10:["Q", "Z"]
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"],
 }
+
 
 def draw_letters():
     letter_pool = convert_dict_to_list(POOL)
@@ -31,33 +52,38 @@ def draw_letters():
         random_array.append(random_letter)
 
     return random_array
+
+
 # =============== helper functions =============
-def convert_dict_to_list(dict):
+def convert_dict_to_list(letter_pool_dict):
     letters = []
-    for letter, quantity in POOL.items():
+    for letter, quantity in letter_pool_dict.items():
         for _ in range(quantity):
             letters.append(letter)
     return letters
 
+
 # ========================================================
+
 
 def uses_available_letters(word, letter_bank):
     """
-        word: string input word
-        letter_bank: array of drawn letters in a hand.
+    word: string input word
+    letter_bank: array of drawn letters in a hand.
     """
     is_available = True
     word_dict = collections.Counter(word.upper())
-    letter_bank_dict = collections.Counter(letter_bank)
+    letter_bank_dict = collections.Counter(letter.upper() for letter in letter_bank)
 
     for letter, count in word_dict.items():
         if letter_bank_dict[letter] >= count:
-            continue  
+            continue
         else:
             return False
 
     return is_available
-    
+
+
 def score_word(word):
     """
     word: string of characters
@@ -67,15 +93,43 @@ def score_word(word):
     _word = word.upper()
 
     for letter in _word:
-        for key,letters in SCORE_WORD.items():
+        for key, letters in SCORE_WORD.items():
             if letter in letters:
-                score+= key
+                score += key
 
-    if len(word)>= 7 and len(word)<= 10:
-        score+= 8
+    if len(word) >= 7 and len(word) <= 10:
+        score += 8
 
     return score
 
 
 def get_highest_word_score(word_list):
-    pass
+    words_score = {}
+
+    for word in word_list:
+        score = score_word(word)
+        if score not in words_score:
+            words_score[score] = []
+        words_score[score].append(word)
+
+    max_score = 0
+    for score in words_score:
+        max_score = max(max_score, score)
+
+    tied_words = words_score[max_score]
+
+    ten_letter_word = None
+    for word in tied_words:
+        if len(word) == 10:
+            ten_letter_word = word
+            break
+
+    if ten_letter_word:
+        winning_word = ten_letter_word
+    else:
+        winning_word = tied_words[0]
+        for word in tied_words:
+            if len(word) < len(winning_word):
+                winning_word = word
+
+    return (winning_word, max_score)
